96977fe87bc06b0ac6c2c19e3a686d0cfca21f89
yedilbaevaidana <47076700+yedilbaevaidana@users.noreply.github.com>
2019-02-15 05:26:43 +0600

    lab2

---
+++ ConsoleApp11/ConsoleApp11/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fn = "thisFile.txt";
+            string pathFrom = @"C:\Users\001\Desktop\PP2 задания";
+            string pathTo = @"C:\Users\001\Desktop\PP2";
+            string whereFrom = Path.Combine(pathFrom, fn);
+            string shouldBe = Path.Combine(pathTo, fn);
+            FileStream fs = File.Create(whereFrom);
+            fs.Close();
+            File.Copy(whereFrom, shouldBe, true);
+            File.Delete(whereFrom);
+        }
+    }
+}
---
+++ ConsoleApp7/ConsoleApp7/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ConsoleApp7
+{
+    class MainClass
+    {
+        static string s = "    ";
+        static void PrintInfo(DirectoryInfo dir, int k)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            for (int i = 0; i < k; i++)
+            {
+                Console.WriteLine(s);
+            }
+            Console.Write(dir.Name);
+            Console.WriteLine();
+            var arr = dir.GetFileSystemInfos();
+            foreach (var t in arr)
+            {
+                if (t.GetType() == typeof(DirectoryInfo))
+                {
+                    DirectoryInfo d = t as DirectoryInfo;
+                    PrintInfo(d, k + 1);
+                }
+                if (t.GetType() == typeof(FileInfo))
+                {
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    for (int i = 0; i < k; i++)
+                    {
+                        Console.WriteLine(s);
+                    }
+                    Console.WriteLine(t.Name);
+                    Console.WriteLine();
+                    Console.ReadKey();
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            DirectoryInfo di = new DirectoryInfo(@"C:\Users\001\Desktop\PP2 задания");
+            PrintInfo(di, 0);
+        }
+    }
+}
